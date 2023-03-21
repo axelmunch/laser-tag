ce508f38ce849411a6aae0668c64488cbcca14a4
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-03-21 21:20:01 +0100

    Configuration variables

---
+++ laser-tag/configuration/__init__.py
@@ -0,0 +1,8 @@
+from .variables import *
+
+VARIABLES = Variables()
+
+VERSION = "v0.0.1"
+GAME_NAME = "Laser Tag"
+
+WINDOW_WINDOWED_SIZE_RATIO = 0.75
---
+++ laser-tag/configuration/variables.py
@@ -0,0 +1,28 @@
+class Variables:
+    def __init__(self):
+        # Default values
+        self.full_screen_width = 1920
+        self.full_screen_height = 1080
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.fullscreen = False
+
+        self.server_port = 16168
+
+        self.fps = 60
+
+        self.pseudo = "Player"
+
+        # Load from file
+        self.load()
+
+    def load(self):
+        pass
+
+    def set_full_screen_size(self, width, height):
+        self.full_screen_width = width
+        self.full_screen_height = height
+
+    def set_screen_size(self, width, height):
+        self.screen_width = width
+        self.screen_height = height
