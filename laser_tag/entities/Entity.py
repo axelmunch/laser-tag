6336fe171d1396f3c470c97850398141fe4399d4
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-03-30 20:29:22 +0200

    Entity class

---
+++ laser_tag/entities/Entity.py
@@ -0,0 +1,11 @@
+class Entity:
+    def __init__(self, x, y, z, length, width, height):
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.length = length
+        self.width = width
+        self.height = height
+
+        self.rotation = 0
