dff05a2b556f268a6354efd6f24033c3f7693259
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-04-04 23:08:03 +0200

    Export and import of events

--- laser_tag/events/EventInstance.py
+++ laser_tag/events/EventInstance.py
@@ -1,5 +1,6 @@
 from time import time
 
+from ..network.safe_eval import safe_eval
 from .Event import Event, local_events
 
 
@@ -9,3 +10,19 @@ class EventInstance:
         self.id = id
         self.data = data
         self.local = id in local_events
+        self.quantity = 1
+        self.time_pressed = 0
+
+    def __repr__(self):
+        return f"[{self.id.value}, {self.data}, {self.quantity}, {self.time_pressed}]"
+
+    @staticmethod
+    def create(string):
+        parsed_event = safe_eval(string)
+        try:
+            event = EventInstance(Event(parsed_event[0]), parsed_event[1])
+            event.quantity = parsed_event[2]
+            event.time_pressed = parsed_event[3]
+            return event
+        except:
+            return None
