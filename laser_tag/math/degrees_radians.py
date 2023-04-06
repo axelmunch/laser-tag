0836c394b1c2d71425b6b352fb9087ad3b57bf88
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-04-06 20:11:51 +0200

    Conversions of radians and degrees

---
+++ laser_tag/math/degrees_radians.py
@@ -0,0 +1,9 @@
+from math import pi
+
+
+def radians_to_degrees(radians):
+    return radians * 180 / pi
+
+
+def degrees_to_radians(degrees):
+    return degrees * pi / 180
--- laser_tag/math/rotations.py
+++ laser_tag/math/rotations.py
@@ -1,4 +1,4 @@
-from math import atan2, cos, pi, sin, sqrt
+from math import atan2, cos, sin
 
 from .degrees_radians import *
 from .Point import Point
