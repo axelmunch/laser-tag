ab2536d98c9a96eae0c2df92f8bf1fceb159fd04
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-04-18 21:19:51 +0200

    Entities have a unique id

--- laser_tag/game/World.py
+++ laser_tag/game/World.py
@@ -1,4 +1,4 @@
-from ..entities import Entity
+from ..entities import GameEntity
 from ..events.Event import Event
 from ..events.EventInstance import EventInstance
 from .Map import Map
@@ -7,9 +7,23 @@ from .Map import Map
 class World:
     def __init__(self, map_id=None):
         self.map = Map(map_id)
-        self.entities = []
+        self.entities = {}
 
         self.controlled_entity = None
+
+        self.current_uid = 0
+
+    def get_uid(self):
+        uid = self.current_uid
+        self.current_uid += 1
+        return uid
+
+    def spawn_entity(self, entity: GameEntity):
+        self.entities[self.get_uid()] = entity
+        return self.current_uid - 1
+
+    def set_controlled_entity(self, uid):
+        self.controlled_entity = uid
 
     def enhance_events(self, events: list[EventInstance]):
         pass
