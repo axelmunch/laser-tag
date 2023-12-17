370b8712b139f07b89959e871e1793b5078822f7
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2023-12-17 14:25:52 +0100

    Line intersection algorithm

--- laser_tag/math/Line.py
+++ laser_tag/math/Line.py
@@ -10,6 +10,46 @@ class Line:
         self.point1 = point1
         self.point2 = point2
 
+    def get_intersection_line(self, line: Line) -> Point | None:
+        """Returns the intersection point between two lines"""
+        x1, y1 = self.point1.x, self.point1.y
+        x2, y2 = self.point2.x, self.point2.y
+        x3, y3 = line.point1.x, line.point1.y
+        x4, y4 = line.point2.x, line.point2.y
+
+        determinant = (x1 - x2) * (y3 - y4) - (x3 - x4) * (y1 - y2)
+
+        # Parallel lines
+        if determinant == 0:
+            return None
+
+        # Get intersection point
+        x = (
+            (x1 * y2 - x2 * y1) * (x3 - x4) - (x1 - x2) * (x3 * y4 - x4 * y3)
+        ) / determinant
+        y = (
+            (x1 * y2 - x2 * y1) * (y3 - y4) - (y1 - y2) * (x3 * y4 - x4 * y3)
+        ) / determinant
+
+        return Point(x, y)
+
+    def get_intersection_segment(self, line: Line) -> Point | None:
+        """Returns the intersection point between two segments"""
+        intersection = self.get_intersection_line(line)
+        if intersection is None:
+            return None
+
+        if (
+            self.point1.x <= intersection.x <= self.point2.x
+            or self.point1.x >= intersection.x >= self.point2.x
+        ) and (
+            self.point1.y <= intersection.y <= self.point2.y
+            or self.point1.y >= intersection.y >= self.point2.y
+        ):
+            return intersection
+
+        return None
+
     def __repr__(self):
         return f"[{self.point1},{self.point2}]"
 
