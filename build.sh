3bb0a6f7fa12339e54feae19c12b1b02359e876f
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2024-02-25 23:02:17 +0100

    Changed compilation scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-pyinstaller --clean --noconsole --workpath build --distpath dist --specpath build --name "Laser Tag" laser_tag/__main__.py
-pyinstaller --clean --workpath build --distpath dist --specpath build --name "Laser Tag Server" laser_tag/network/Server.py
+pyinstaller --clean --onefile --noconsole --workpath build --distpath dist --specpath build --name "Laser Tag" laser_tag/__main__.py
+cp -r data dist
+
+pyinstaller --clean --onefile --workpath build --distpath dist --specpath build --name "Laser Tag Server" laser_tag/network/Server.py
